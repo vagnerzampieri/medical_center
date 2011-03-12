924fe4f7aafd7598e58c4a4c1dcf19c1751f2b58
Vagner Zampieri <vgzampieri@gmail.com>
2011-03-12 10:21:29 -0300

    Modificado layout do form de médico

--- app/helpers/admin/doctors_helper.rb
+++ app/helpers/admin/doctors_helper.rb
@@ -10,4 +10,14 @@ module Admin::DoctorsHelper
     raw specialties_td
   end
   
+  def show_health_plan(doctor)
+    health_plans_td = "<td>"
+    doctor.health_plan.each do |health_plan|
+      health_plans_td << health_plan.name
+      health_plans_td << "<br/>"
+    end
+    health_plans_td << "</td>"
+    raw health_plans_td
+  end
+  
 end
