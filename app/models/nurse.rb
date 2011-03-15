95130d4ce0c12e2cab89fa6dbf2fd597efbb0a28
Vagner Zampieri <vgzampieri@gmail.com>
2011-03-14 23:02:19 -0300

    Implementado especialidade de enfermagem

--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -1,3 +1,10 @@
 class Nurse < Servant
   has_and_belongs_to_many :specifications
+  
+  def save_specifications(specifications)
+    specifications.each_value do |id|
+      specification = Specification.find id
+      self.specifications << specification
+    end
+  end
 end
